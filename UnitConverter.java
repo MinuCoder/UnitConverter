215949c98a46e1838276e902aba8c0bbedcadb33
SkyMinu <guyio3927@gmail.com>
2017-07-27 10:12:42 +0900

    Add files via upload

---
+++ UnitConverter.java
@@ -0,0 +1,35 @@
+/**
+ * Created by 민우 on 2017-07-20.
+ */
+public class UnitConverter {
+    // 필요한 상수 정의
+    public static final double KILOGRAMS_PER_POUND = 0.45359237;
+    public static final double POUNDS_PER_KILOGRAM = 1 / KILOGRAMS_PER_POUND;
+    public static final double CENTIMETERS_PER_INCH = 2.54;
+    public static final double INCHES_PER_CENTIMETE = 1 / CENTIMETERS_PER_INCH;
+//    public static final double FAHRENHEIT_PER_CENTIMETERS =
+
+    public static double toPounds(double kilograms) {
+        return POUNDS_PER_KILOGRAM*kilograms;
+    }
+
+    public static double toKilograms(double pounds) {
+        return KILOGRAMS_PER_POUND*pounds;
+    }
+
+    public static double toCentimeters(double inches) {
+        return CENTIMETERS_PER_INCH*inches;
+    }
+
+    public static double toInches(double centimeters) {
+        return INCHES_PER_CENTIMETE*centimeters;
+    }
+
+    public static double toFahrenheit(double celsius) {
+        return celsius*9/5+32;
+    }
+
+    public static double toCelsius(double fahrenheit) {
+        return (fahrenheit-32)*5/9;
+    }
+}
